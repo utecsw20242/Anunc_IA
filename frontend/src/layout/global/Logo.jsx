0947b1b2c8cfcb1fe9225a79197e83af97f0f548
christophernajarro <christopher.najarro@utec.edu.pe>
2024-10-30 19:58:10 -0500

    Logo

--- frontend/src/layout/global/Logo.jsx
+++ frontend/src/layout/global/Logo.jsx
@@ -4,8 +4,8 @@ import { twMerge } from 'tailwind-merge'
 function Logo({ rootClassName, className, classNameDark, classNameWhite }) {
     return (
         <div className={twMerge('flex items-center', rootClassName)}>
-            <img className={twMerge('h-16 dark:hidden', className, classNameDark)} src="/logo.png" alt="Logo" />
-            <img className={twMerge('h-16 hidden dark:block', className, classNameWhite)} src="/logo-white.png" alt="Logo Blanco" />
+            <img className={twMerge('h-20 dark:hidden', className, classNameDark)} src="/logo.png" alt="Logo" />
+            <img className={twMerge('h-20 hidden dark:block', className, classNameWhite)} src="/logo-white.png" alt="Logo Blanco" />
         </div>
     );
 }
